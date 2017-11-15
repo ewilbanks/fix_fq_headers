5faf3c8af3ad861a08fc71b5ee6246ac426a27d3
ewilbanks <ewilbanks@128-111-61-153.vpn.ucsb.edu>
2017-11-14 16:58:10 -0800

    foo

--- fq_fix_index.pl
+++ fq_fix_index.pl
@@ -9,8 +9,8 @@ die "usage: fq_fix_index.pl infile.fastq\n Pulls indexes out of header of fastq 
 open(IN, "< $ARGV[0]") or die "can't open file $ARGV[0]\n";
 while (my $line = <IN>) {
   #	if ($line =~ /^@([^\s]+) \d:N:\d:([A-Z]+)\+([A-Z]+)/){
-	if ($line =~ /^@([^\s]+) \d:N:\d:([A|T|C|G]+)\+([A|T|C|G]+)/){
-    print "$1\t$2\t$3\n";
+	if ($line =~ /^(@[^\s]+) \d:N:\d:([A|T|C|G]+)\+([A|T|C|G]+)/){
+    print "$1\n\$2\n$1\n\$3\n";
   }
 }
 close(IN);
