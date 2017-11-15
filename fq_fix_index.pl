a27d32093714b57bb8f27b212ebb2be99526ce2d
ewilbanks <ewilbanks@128-111-61-153.vpn.ucsb.edu>
2017-11-14 17:04:32 -0800

    foo

--- fq_fix_index.pl
+++ fq_fix_index.pl
@@ -7,10 +7,17 @@ use warnings;
 die "usage: fq_fix_index.pl infile.fastq\n Pulls indexes out of header of fastq and makes them their own file" unless @ARGV==1;
 
 open(IN, "< $ARGV[0]") or die "can't open file $ARGV[0]\n";
+my @filename = split(/\.fastq/, $ARGV[0]);
+my $base = $filename[0];
+print "$base\n";
+
+#open(OUT1,">$outFastq");
+#open(OUT2)
 while (my $line = <IN>) {
   #	if ($line =~ /^@([^\s]+) \d:N:\d:([A-Z]+)\+([A-Z]+)/){
 	if ($line =~ /^(@[^\s]+) \d:N:\d:([A|T|C|G]+)\+([A|T|C|G]+)/){
-    print "$1\n$2\n$1\n$3\n";
+    print  "$1\n$2\n";
+    print  "$1\n$3\n";
   }
 }
 close(IN);
